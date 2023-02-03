ecd8e08a34c95c5656a961506dad08dcf4a763ea
tungkthd01 <phanthanhtung199801@gmail.com>
2023-02-03 09:23:17 +0700

    feat: mai bat dau code 1 trang web:

--- api/main.py
+++ api/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 @app.get('/')
 async def root():
     return {'message': 'reload'}
+
+@app.post('/createpost')
+def create_post(new_post: Post):
+    print("payload", new_post)
+    return {'message': new_post}
